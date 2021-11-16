2f87b35d4736b163748a19c6d5637b9927e6a4e4
Yunus Oktay <ynsokty.3@gmail.com>
2021-11-16 21:09:31 +0300

    Initial Commit

---
+++ UyariMesajlari/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UyariMesajlari
+//
+//  Created by yunus oktay on 16.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
